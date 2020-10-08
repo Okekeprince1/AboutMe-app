ced70bcb953dd9ff6220f0015f95fd42cd087234
Okekeprince1 <okeke.p.ebube@gmail.com>
2020-10-08 13:06:18 +0100

    Implemented data binding works perfectly

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -5,32 +5,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //Data bing variable declaration
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //Data bing variable initialization
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val donBtn: Button = findViewById(R.id.done)
-
-
-        donBtn.setOnClickListener{ changeNickname(it) }
+        binding.done.setOnClickListener{ changeNickname() }
     }
 
-    private fun changeNickname(view: View){
-        val nicknameText: TextView = findViewById(R.id.nickName)
-        val nicknameEditText: EditText = findViewById(R.id.editNickName)
-        nicknameText.text = nicknameEditText.text
-
-        nicknameEditText.visibility = View.GONE
-        view.visibility = View.GONE
-        nicknameText.visibility = View.VISIBLE
+    private fun changeNickname(){
+        binding.nickName.text = binding.editNickName.text.toString()
+        binding.editNickName.visibility = View.GONE
+        binding.done.visibility = View.GONE
+        binding.nickName.visibility = View.VISIBLE
 
         // Hide the keyboard.
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(view.windowToken, 0)
+        imm.hideSoftInputFromWindow(binding.done.windowToken, 0)
     }
 }
